8f3391d56c72fdf142112c6f9995d40590fd9af0
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-03-07 15:41:27 -0800

    add error handling middleware

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.use(bodyParser.json());
 
 app.use('/', router);
 
+app.use((err, req, res, next) => {
+    if(!err.status){
+        err.status = 501;
+    }
+    res.status(err.status).send(err.message);
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
 });
