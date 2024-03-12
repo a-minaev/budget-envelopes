eaa1a893f8b92fc799cd627e48c487a2901cc5af
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-03-11 17:06:45 -0700

    add queries for balance verification, withdrawls, and transfers

--- database/queries.js
+++ database/queries.js
@@ -29,5 +29,44 @@ const deposit = ((req, res, next) => {
 })
 
 const verifyBalance = ((req, res, next) => {
-    
+    //add functionality for transfers
+
+    pool.query('SELECT * FROM envelopes WHERE id=$1 AND amount>=$2', [req.id, req.query.amount], (error, results) => {
+        if(error){
+            next(error);
+        }
+        else next();
+    })
 })
+
+const withdrawAmount = ((req, res, next) => {
+    pool.query('UPDATE envelopes SET amount=amount-$1 WHERE id=$2', [req.id, req.query.amount], (error, results) =>{
+        if(error){
+            next(error);
+        }
+        else res.status(200).send(`Succesfully withdrew amount, you have ${results.rows.amount} remaining in ${results.rows.name}`)
+    })
+})
+
+const transferAmount = ((req, res, next) => {
+    const transferFrom = req.query.accountFrom;
+    const transferTo = req.query.accountTo;
+
+    pool.query('UPDATE envelopes SET amount=amount-$1 WHERE id=$2', [req.query.amount, transferFrom], (error, results) => {
+        if(error){
+            next(error);
+        }
+    })
+    pool.query('UPDATE envelopes SET amount=amount+$1 WHERE id=$2', [req.query.amount, transferTo], (error, results) => {
+        if(error){
+            next(error);
+        }
+        else res.status(200).send('Transfer successfull!');
+    })
+})
+
+
+
+
+
+module.exports = { getEnvelopes, getEnvelope, deposit, verifyBalance, withdrawAmount, transferAmount }
