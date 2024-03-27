279f9661ede11911b264c5e9b0a0c25a05cdc504
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-03-27 14:47:12 -0700

    add logging to verifyBalance and update syntax in withdraw query

--- database/queries.js
+++ database/queries.js
@@ -37,12 +37,16 @@ const deposit = ((req, res, next) => {
 
 const verifyBalance = ((req, res, next) => {
     //add functionality for transfers
+    console.log(`verifyBalance is executing with req.id:${req.id}`)
     if(req.id){
         pool.query('SELECT * FROM envelopes WHERE id=$1 AND amount>=$2', [req.id, req.query.amount], (error, results) => {
             if(error){
                 next(error);
             }
-            else next();
+            else{
+                console.log('succsesfully verified balance');
+                next()
+            };
         })
     } else{
         pool.query('SELECT * FROM envelopes WHERE id=$1 AND amount>=$2', [req.query.accountFrom, req.query.amount], (error, results) => {
@@ -55,7 +59,7 @@ const verifyBalance = ((req, res, next) => {
 })
 
 const withdrawAmount = ((req, res, next) => {
-    pool.query('UPDATE envelopes SET amount=amount-$1 WHERE id=$2', [req.id, req.query.amount], (error, results) =>{
+    pool.query('UPDATE envelopes SET amount=(amount-$2) WHERE id=$1', [req.id, req.query.amount], (error, results) =>{
         if(error){
             next(error);
         }
@@ -67,12 +71,12 @@ const transferAmount = ((req, res, next) => {
     const transferFrom = req.query.accountFrom;
     const transferTo = req.query.accountTo;
 
-    pool.query('UPDATE envelopes SET amount=amount-$1 WHERE id=$2', [req.query.amount, transferFrom], (error, results) => {
+    pool.query('UPDATE envelopes SET amount=(amount-$1) WHERE id=$2', [req.query.amount, transferFrom], (error, results) => {
         if(error){
             next(error);
         }
     })
-    pool.query('UPDATE envelopes SET amount=amount+$1 WHERE id=$2', [req.query.amount, transferTo], (error, results) => {
+    pool.query('UPDATE envelopes SET amount=(amount+$1) WHERE id=$2', [req.query.amount, transferTo], (error, results) => {
         if(error){
             next(error);
         }
